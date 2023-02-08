fda1b8d4e4b9b3d888df202c59289f9c79fd09d2
Polina <polinaandr123@yandex.com>
2023-02-08 17:09:16 +0300

    update window.py

--- window.py
+++ window.py
@@ -1,35 +1,41 @@
-from PyQt5.QtWidgets import (
-    QMainWindow,
-)
-from PyQt5.QtGui import QPixmap, QKeyEvent
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QMainWindow,)
+from PyQt5.QtGui import QPixmap, QKeyEvent
 from static_maps_api import get_map
 from window_ui import Ui_MainWindow
-
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
         self._map_zoom = 5
-        self._map_ll = 37.977751, 55.757718
+        self._map_ll = [37.977751, 55.757718]
         self._map_l = 'map'
         self._init_ui()
-
     def _init_ui(self) -> None:
         self._refresh_map()
-
-    def keyPressEvent(self, event: QKeyEvent):
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        move_distance = 35 / self._map_zoom ** 2
         previous_zoom = self._map_zoom
+        previous_ll = self._map_ll.copy()
         if event.key() == Qt.Key_PageUp:
             self._map_zoom += 1
         elif event.key() == Qt.Key_PageDown:
             self._map_zoom -= 1
-        self._map_zoom = self._clip_zoom(self._map_zoom, 1, 16)
-        if previous_zoom != self._map_zoom:
-            self._refresh_map()
-
-    def _clip_zoom(self, zoom, min_zoom, max_zoom):
+        elif event.key() == Qt.Key_Up or event.key() == Qt.Key_W:
+            self._map_ll[1] += move_distance
+        elif event.key() == Qt.Key_Down or event.key() == Qt.Key_S:
+            self._map_ll[1] -= move_distance
+        elif event.key() == Qt.Key_Left or event.key() == Qt.Key_A:
+            self._map_ll[0] -= move_distance
+        elif event.key() == Qt.Key_Right or event.key() == Qt.Key_D:
+            self._map_ll[0] += move_distance
+            self._map_zoom = self._clip_zoom(self._map_zoom, 1, 16)
+        if previous_zoom == self._map_zoom and previous_ll == self._map_ll:
+            return
+        self._refresh_map()
+    def _clip_zoom(self, zoom: int, min_zoom: int, max_zoom: int) -> int:
         if zoom < min_zoom:
             return min_zoom
         if zoom > max_zoom:
@@ -37,12 +43,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         return zoom
     def _refresh_map(self) -> None:
         image_data = get_map(ll=self._map_ll, zoom=self._map_zoom, map_type=self._map_l)
-        # создание временного файла (плохой способ)
-        # with NamedTemporaryFile('wb', prefix='qt-image-', suffix='.png') as file:
-        #     file.write(image_data)
-        #     file.flush()
-        #     pixmap = QPixmap(file.name)
-        #     self.map_label.setPixmap(pixmap)
         pixmap = QPixmap()
         pixmap.loadFromData(image_data)
         self.map_label.setPixmap(pixmap)
